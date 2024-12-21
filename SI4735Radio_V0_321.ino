1fc8c372343cf9ba3b1368014cc1ad55d753c92f
CRSourcing <55854843+CRSourcing@users.noreply.github.com>
2024-12-21 06:09:36 -0600

    Update SI4735Radio_V0_321.ino

--- SI4735Radio_V0_321.ino
+++ SI4735Radio_V0_321.ino
@@ -510,8 +510,8 @@ uint16_t buttonSelected = 1;  // 0-7 are available
 // Web
 uint8_t imageSelector = 0;
 bool swappedJPEG = false;
-const char* ssid = "IguanaAlemana";
-const char* password = "rembremerding";
+const char* ssid = "YourSSID";
+const char* password = "YourPW";
 int yShift = 0;
 int xShift = 0;
 int reportSelector = 0;
